a6b5185222ff1273ef72fa834a6e291d53fa6a4f
jbutki-coder <jakebutki@gmail.com>
2026-05-30 00:52:50 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = “virtual-na-meeting-finder-v1”;
+
+const FILES_TO_CACHE = [
+“/”,
+“/index.html”,
+“/manifest.json”,
+“/icon-192.png”,
+“/icon-512.png”
+];
+
+self.addEventListener(“install”, event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(FILES_TO_CACHE);
+})
+);
+});
+
+self.addEventListener(“fetch”, event => {
+event.respondWith(
+fetch(event.request).catch(() => caches.match(event.request))
+);
+});
